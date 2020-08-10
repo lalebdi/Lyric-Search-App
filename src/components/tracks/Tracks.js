1ebcb0bccce1dc91690a637f46607a963af04d46
lalebdi <loai.alebdi@gmail.com>
2020-08-10 15:18:40 -0400

    spinner works

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import { Consumer } from '../../Context';
+import Spinner from '../layout/Spinner';
 
 class Tracks extends Component {
     render() {
         return (
             <Consumer>
                 {value => {
-                    console.log(value);
-                    return <h1>Tracks</h1>
+                    // console.log(value);
+                    const { track_list } = value;
+                    if(track_list === undefined || track_list.length === 0){
+                            return <Spinner />
+                    }else{
+                        return <h1>Tracks loaded</h1>
+                    }
                 }}
             </Consumer>
         )
